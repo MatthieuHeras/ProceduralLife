dc34e8bb4be2bfc95f1a5ecf402bfe0bfc4c4778
m-heras <m.heras@ishtar.games>
2024-03-02 12:07:26 +0100

    Add Command Pattern

---
+++ ProceduralLife/Assets/Scripts/MHLib/CommandPattern/ACommand.cs
@@ -0,0 +1,9 @@
+﻿namespace MHLib.CommandPattern
+{
+    public abstract class ACommand
+    {
+        public abstract void Do();
+        public abstract void Undo();
+        public virtual void Redo() => this.Do();
+    }
+}
---
+++ ProceduralLife/Assets/Scripts/MHLib/CommandPattern/CommandLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using MHLib.Extensions;
+
+namespace MHLib.CommandPattern
+{
+    public class CommandLinkedList<TCommand>
+        where TCommand : ACommand
+    {
+        private readonly LinkedList<TCommand> linkedList = new();
+        private LinkedListNode<TCommand> currentNode;
+        private LinkedListNode<TCommand> nextNode;
+        
+        public void Do(TCommand newCommand)
+        {
+            LinkedListNode<TCommand> newNode = new(newCommand);
+            
+            if (this.currentNode != null)
+                this.linkedList.AddAfter(this.currentNode, newNode);
+            else
+                this.linkedList.AddFirst(newNode);
+            
+            this.currentNode = newNode;
+            this.nextNode = null;
+            
+            // Clean the linked list tail when we Undo then Do (and not Redo)
+            this.currentNode.RemoveAllAfter();
+            
+            newCommand.Do();
+        }
+
+        public void Undo()
+        {
+            if (this.currentNode == null)
+                return;
+            
+            this.currentNode.Value.Undo();
+            
+            this.nextNode = this.currentNode;
+            this.currentNode = this.currentNode.Previous;
+        }
+        
+        public void Redo()
+        {
+            if (this.nextNode == null)
+                return;
+            
+            this.nextNode.Value.Redo();
+            
+            this.currentNode = this.nextNode;
+            this.nextNode = this.nextNode.Next;
+        }
+    }
+}
