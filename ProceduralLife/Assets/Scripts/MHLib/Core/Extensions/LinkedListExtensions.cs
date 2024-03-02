0dfc285df5e6cb88d4f16e931b7a72cd28378c64
m-heras <m.heras@ishtar.games>
2024-03-02 12:07:13 +0100

    Add LinkedList extensions

---
+++ ProceduralLife/Assets/Scripts/MHLib/Core/Extensions/LinkedListExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace MHLib.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class LinkedListExtensions
+    {
+        public static void RemoveAllAfter<T>(this LinkedListNode<T> node)
+        {
+            while (node.Next != null)
+                node.List.Remove(node.Next);
+        }
+        
+        public static void RemoveAllBefore<T>(this LinkedListNode<T> node)
+        {
+            while (node.Previous != null)
+                node.List.Remove(node.Previous);
+        }
+    }
+}
