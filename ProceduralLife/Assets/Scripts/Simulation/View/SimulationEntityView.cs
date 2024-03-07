e8e5ceb4ae222c438f8d3501f7950cae6ac3d566
m-heras <m.heras@ishtar.games>
2024-03-07 21:14:14 +0100

    Turn the sheep towards where it moves

--- ProceduralLife/Assets/Scripts/Simulation/View/SimulationEntityView.cs
+++ ProceduralLife/Assets/Scripts/Simulation/View/SimulationEntityView.cs
@@ -14,7 +14,12 @@ namespace ProceduralLife.Simulation.View
         {
             Vector3 worldPosition = HexagonHelper.TileToWorld(newPosition, Constants.TILE_SIZE);
             
-            this.transform.position = worldPosition;
+            Transform selfTransform = this.transform;
+            
+            Vector3 positionDiff = worldPosition - selfTransform.position;
+            float rotation = Mathf.Atan2(positionDiff.z, positionDiff.x) / Mathf.PI * 180f;
+            selfTransform.position = worldPosition;
+            selfTransform.rotation = Quaternion.Euler(0f, -rotation, 0f);
         }
     }
 }
