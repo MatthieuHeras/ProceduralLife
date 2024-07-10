0c645feb258827fb6ca052da72cddd6c9688c3ab
m-heras <m.heras@ishtar.games>
2024-07-10 19:15:47 +0200

    Remove MHLib from project, as it has been transferred to its own repository

--- ProceduralLife/Assets/Scripts/MHLib/CommandPattern/ACommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MHLib.CommandPattern
-{
-    public abstract class ACommand
-    {
-        public abstract void Do();
-        public abstract void Undo();
-        public virtual void Redo() => this.Do();
-    }
-}
