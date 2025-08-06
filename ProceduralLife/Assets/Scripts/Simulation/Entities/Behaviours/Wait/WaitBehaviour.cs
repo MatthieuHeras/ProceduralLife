cc4388fffdc7f51f9439291fddcf5fe2bd27c0ad
m-heras <mheraspro@gmail.com>
2025-08-06 14:45:57 +0200

    Add Wait behaviour

---
+++ ProceduralLife/Assets/Scripts/Simulation/Entities/Behaviours/Wait/WaitBehaviour.cs
@@ -0,0 +1,14 @@
+﻿namespace ProceduralLife.Simulation
+{
+    public class WaitBehaviour : ABehaviour
+    {
+        public WaitBehaviour(BehaviourContext context, ulong parameter) : base(context)
+        {
+            this.parameter = parameter;
+        }
+        
+        private readonly ulong parameter;
+
+        protected override AState GetNewState() => new WaitState(this.entity, this.parameter);
+    }
+}
---
+++ ProceduralLife/Assets/Scripts/Simulation/Entities/Behaviours/Wait/WaitBehaviourDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProceduralLife.Simulation
+{
+    [CreateAssetMenu(fileName = "Wait", menuName = Constants.Editor.PATH_BEHAVIOURS + "Wait")]
+    public class WaitBehaviourDefinition : ABehaviourDefinition
+    {
+        public override Type ParameterType => typeof(ulong);
+
+        public override ABehaviour GetBehaviour(BehaviourContext context, object parameter) => new WaitBehaviour(context, (ulong)parameter);
+    }
+}
